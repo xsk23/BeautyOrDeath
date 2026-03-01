87be4cc18a3ab90dbc8598df5518ec9e396f3465
xsk23 <xsk23@mails.tsinghua.edu.cn>
2026-03-01 14:57:41 +0800

    新增动画*2

--- Assets/Scripts/UI/extract_bgm.py
+++ Assets/Scripts/UI/extract_bgm.py
@@ -37,11 +37,11 @@ def extract_audio_segment(video_path, output_audio_path, start_time, end_time=No
 # --- 使用示例 ---
 
 # 路径
-input_video = r"E:\downloads\b\qiaokeli.mp4"
-output_audio = r"E:\downloads\b\qiaokeli.mp3"
+input_video = r"E:\downloads\b\leisai.mp4"
+output_audio = r"E:\downloads\b\leisai_extracted_audio.mp3"
 
 # 示例 1: 从第 5 秒开始，到第 15 秒结束
-extract_audio_segment(input_video, output_audio, start_time=0, end_time=20)
+extract_audio_segment(input_video, output_audio, start_time=0, end_time=16)
 
 # 示例 2: 从第 1 分 20 秒开始，直到视频结束
 # extract_audio_segment(input_video, output_audio, start_time="00:01:20")
